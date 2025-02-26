dd627996f56c1047e55bfb75ce566d07fe68d031
Javiergonb <javiergonb3@gmail.com>
2025-02-26 15:29:10 -0300

    feat: deleting an entry is reflected un the DB. Also added seperate error handling. Finally fixed a small bug in the front end where deleting an entry didnt reset the component

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
 
-morgan.token("info", (req) => req.info);
+morgan.token("info", (request) => request.info);
 app.use(morgan(":method :url :response-time ms :info"));
 
-const getBody = (req, res, next) => {
-    req.info = JSON.stringify(req.body);
+const getBody = (request, response, next) => {
+    request.info = JSON.stringify(request.body);
     next();
 };
 app.use(getBody);
@@ -24,68 +24,92 @@ let persons = [
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
-  })
+})
 
-app.get("/api/info", (req, res) => {
+app.get("/api/info", (request, response) => {
     const numberOfPeople = persons.length;
     const time = new Date();
-    res.send(
+    response.send(
         `<p>Phonebook has info for ${numberOfPeople} people</p>\n<p>${time}</p>`
     );
 });
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (request, response,next) => {
     Person.findById(request.params.id).then(person => {
-        response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => {
+        console.log(error)
+        next(error)
     })
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter((person) => person.id !== id);
-    res.status(204).end();
+app.delete("/api/persons/:id", (request, response) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(500).end()
+        })
 });
 
 const generateId = (max = 1e12) => String(Math.floor(Math.random() * max));
 
-app.post("/api/persons", (req, res) => {
-    const body = req.body;
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
 
     if (!body.name) {
-        return res.status(400).json({ error: "name missing" });
+        return response.status(400).json({ error: "name missing" });
     }
     if (!body.number) {
-        return res.status(400).json({ error: "number missing" });
+        return response.status(400).json({ error: "number missing" });
     }
 
-    const personExists = persons.some(
-        (person) => person.name === body.name
-    );
-    if (personExists) {
-        return res.status(400).json({ error: "name must be unique" });
-    }
+    //const personExists = persons.some(
+    //    (person) => person.name === body.name
+    //);
+    //if (personExists) {
+    //    return response.status(400).json({ error: "name must be unique" });
+    //}
 
-    const newPerson = {
-        id: generateId(),
+    const newPerson = new Person({
         name: body.name,
-        number: body.number,
-    };
+        number: body.number
+    })
 
-    persons = persons.concat(newPerson);
-    res.json(newPerson);
+    newPerson.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 });
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
